501905f387a1cbb8b98af0186e3b17f562d2d90a
Woody Wu <woody.wu2@honeywell.com>
2019-08-01 13:18:50 +0800

    refine connection method

--- components/SprintViz.js
+++ components/SprintViz.js
@@ -2,11 +2,11 @@ import {useEffect} from 'react';
 import * as d3 from 'd3';
 import Jira from '../lib/jira';
 
-//const jiraHost = 'acsjira.honeywell.com';
-const jiraHost = 'localhost';
-const jiraPort = 8001;
-const jiraUser = 'h316896';
-const jiraPassword = 'HON567well';
+const jiraHost = 'localhost:8001';
+/*
+const jiraHost = 'acsjira.honeywell.com';
+const jiraUser = 'h316896', jiraPassword = 'HON567well';
+*/
 const containerId = 'sprintviz';
 
 function SprintViz()
@@ -23,17 +23,16 @@ function SprintViz()
 
 function draw()
 {
-    console.log('**001 draw');
     const jira = Jira(jiraHost, {
         /*
         user: jiraUser,
         password: jiraPassword,
+        proto: 'https',
         */
         proto: 'http',
-        port: jiraPort,
     });
     jira.issuesGraph('Sprint 10').then(graph => {
-        console.log('sprint loaded');
+        console.log('sprint loaded', graph);
     });
 }
 
